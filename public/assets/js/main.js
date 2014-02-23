73af54979a646f79e2544eab507e7983d78f6bc5
Raymond van Broeck <deslaper@gmail.com>
2014-02-23 18:50:45 +0100

    created a simple html / js chat

---
+++ public/assets/js/main.js
@@ -0,0 +1,34 @@
+var user_id = Math.random();
+
+var conn = new WebSocket('ws://localhost:8080');
+
+conn.onopen = function(e) {
+	console.log('Connection enabled');
+}
+
+conn.onmessage = function(e) {
+	console.log(e.data);
+	addMsg(e.data);
+}
+
+function addMsg(msg) {
+	var entry = $('<span />').addClass('h').html(msg);
+	$('#chatWindow > span.h').append(entry);
+}
+
+$(function()
+{
+	
+
+	var sendo = function()
+	{
+		var $this = $('#msg'),
+		    message = $this.val();
+	
+		conn.send(message);	
+		addMsg(message);
+		$this.val('');
+	}
+
+	$(document).on('submit', '#frm', sendo);
+});
